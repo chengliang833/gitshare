9ab08ef34a4403f03aec4d3959dcc80cd1e7c6a4
程亮 <cl@ulane.cn>
2021-11-28 16:46:13 +0800

    netsh 修改

---
+++ window_excute/netshproxyportadd.bat
@@ -0,0 +1,6 @@
+::在这添加吧
+netsh interface portproxy add v4tov4 listenport=1080 listenaddress=192.168.5.1 connectaddress=127.0.0.1 connectport=1080
+netsh interface portproxy add v4tov4 listenport=10808 listenaddress=192.168.5.1 connectaddress=127.0.0.1 connectport=10808
+netsh interface portproxy add v4tov4 listenport=10809 listenaddress=192.168.5.1 connectaddress=127.0.0.1 connectport=10809
+netsh interface portproxy add v4tov4 listenport=10810 listenaddress=192.168.5.1 connectaddress=127.0.0.1 connectport=10810
+pause
---
+++ window_excute/netshproxyportdelete.bat
@@ -0,0 +1,5 @@
+netsh interface portproxy delete v4tov4 listenport=1080 listenaddress=192.168.5.1
+netsh interface portproxy delete v4tov4 listenport=10810 listenaddress=192.168.5.1
+netsh interface portproxy delete v4tov4 listenport=10809 listenaddress=192.168.5.1
+netsh interface portproxy delete v4tov4 listenport=10808 listenaddress=192.168.5.1
+pause
