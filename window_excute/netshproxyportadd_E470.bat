c28ca5b302ee985152f56e6586f2461b29bcff08
程亮 <cl@ulane.cn>
2022-04-23 13:42:20 +0800

    mouseInc版本更新

--- window_excute/netshproxyportadd_E470.bat
+++ window_excute/netshproxyportadd_E470.bat
@@ -1,8 +1,9 @@
-::在这添加吧
+::在这添加吧，要开热点之后重新执行
 netsh interface portproxy add v4tov4 listenport=1080 listenaddress=192.168.126.1 connectaddress=127.0.0.1 connectport=1080
 netsh interface portproxy add v4tov4 listenport=10808 listenaddress=192.168.126.1 connectaddress=127.0.0.1 connectport=10808
 netsh interface portproxy add v4tov4 listenport=10809 listenaddress=192.168.126.1 connectaddress=127.0.0.1 connectport=10809
 netsh interface portproxy add v4tov4 listenport=10810 listenaddress=192.168.126.1 connectaddress=127.0.0.1 connectport=10810
-netsh interface portproxy add v4tov4 listenport=53389 listenaddress=192.168.137.1 connectaddress=192.168.204.132 connectport=3389
 netsh interface portproxy add v4tov4 listenport=2181 listenaddress=127.0.0.1 connectaddress=192.168.204.128 connectport=2181
+netsh interface portproxy add v4tov4 listenport=53389 listenaddress=192.168.137.1 connectaddress=192.168.204.132 connectport=3389
+netsh interface portproxy add v4tov4 listenport=58888 listenaddress=192.168.137.1 connectaddress=192.168.204.132 connectport=8888
 pause
--- window_excute/netshproxyportdelete_E470.bat
+++ window_excute/netshproxyportdelete_E470.bat
@@ -1,6 +1,9 @@
+::删除
 netsh interface portproxy delete v4tov4 listenport=1080 listenaddress=192.168.126.1
-netsh interface portproxy delete v4tov4 listenport=10810 listenaddress=192.168.126.1
-netsh interface portproxy delete v4tov4 listenport=10809 listenaddress=192.168.126.1
 netsh interface portproxy delete v4tov4 listenport=10808 listenaddress=192.168.126.1
+netsh interface portproxy delete v4tov4 listenport=10809 listenaddress=192.168.126.1
+netsh interface portproxy delete v4tov4 listenport=10810 listenaddress=192.168.126.1
+netsh interface portproxy delete v4tov4 listenport=2181 listenaddress=127.0.0.1
 netsh interface portproxy delete v4tov4 listenport=53389 listenaddress=192.168.137.1
+netsh interface portproxy delete v4tov4 listenport=58888 listenaddress=192.168.137.1
 pause
